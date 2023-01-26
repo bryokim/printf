6afd5693c7fe78bf071e054111f6716982bda6a7
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2023-01-26 15:56:32 +0300

    Added custom specifiers

---
+++ custom_specifiers.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+*rot13 - encodes a string using rot13.
+*@str: main string.
+*@s: string to encode.
+*Return: none.
+*/
+void rot13(char *str, char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		while ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+		{
+			str[i] = s[i] + 13;
+			i++;
+		}
+
+		if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+			str[i] = s[i] - 13;
+	}
+}
+
+/**
+*print_rev - gives reverse of a string.
+*@s: string to reverse.
+*@str: main string.
+*Return: none.
+*/
+void print_rev(char *s, char *str)
+{
+	_strcpy(s, str);
+	_strrev(s);
+}
